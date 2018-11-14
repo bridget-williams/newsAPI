48136b2305c0a59476a11e9ba4ffec08a5ca7cd4
Bridget Williams <bridget_williams1@unc.edu>
2018-11-14 12:48:31 -0500

    Initialized readme

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,21 @@ $(document).ready(function(){
     var myKey = config.MY_KEY;
 
     var url = "https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-14&sortBy=publishedAt&apiKey=" + myKey;
-    
+    var data = [];
+    var html = '';
+
+    $.ajax({
+        type:'GET',
+        url: url,
+        dataType: 'json',
+        async: true,
+        data: data,
+        success: function (data){
+            console.log(data);
+        }
+
+
+    });
 
 
 });
